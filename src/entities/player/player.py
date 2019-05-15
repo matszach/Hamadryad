3a46a00a10111d59b602ca518bc977de5023475c
Lukasz <lkaszubowski@gmail.com>
2019-05-16 00:55:22 +0200

    implemented player action "cool-downs"

--- src/entities/player/player.py
+++ src/entities/player/player.py
@@ -4,13 +4,27 @@ import src.entities.spawner as spawner
 
 class PlayerEntity(GroundCharacterEntity):
 
+    ac1cd = 0
+    ac2cd = 0
+
+    def work(self):
+        self.ac1cd = self.ac1cd - 1 if self.ac1cd else 0
+        self.ac2cd = self.ac2cd - 1 if self.ac2cd else 0
+
     def action_1(self):
-        spd_x = -15 if self.sprite_handler.should_flip else +15
-        spawner.spawn_projectile(ProjectileEntity(init_x=self.x, init_y=self.y, spd_x=spd_x, spd_y=-1, size=10))
+        if not self.ac1cd:
+            self.ac1cd = 10
+            spd_x = -15 if self.sprite_handler.should_flip else +15
+            spawner.spawn_projectile(ProjectileEntity(init_x=self.x, init_y=self.y, spd_x=spd_x, spd_y=-1, size=10))
 
     def action_2(self):
-        spd_x = -15 if self.sprite_handler.should_flip else +15
-        spawner.spawn_projectile(ProjectileEntity(init_x=self.x, init_y=self.y, spd_x=spd_x, spd_y=0))
-        spawner.spawn_projectile(ProjectileEntity(init_x=self.x, init_y=self.y, spd_x=spd_x, spd_y=-2))
-        spawner.spawn_projectile(ProjectileEntity(init_x=self.x, init_y=self.y, spd_x=spd_x, spd_y=-4))
+        if not self.ac2cd:
+            self.ac2cd = 30
+            spd_x = -15 if self.sprite_handler.should_flip else +15
+            spawner.spawn_projectile(ProjectileEntity(init_x=self.x, init_y=self.y, spd_x=spd_x, spd_y=0))
+            spawner.spawn_projectile(ProjectileEntity(init_x=self.x, init_y=self.y, spd_x=spd_x, spd_y=-3))
+            spawner.spawn_projectile(ProjectileEntity(init_x=self.x, init_y=self.y, spd_x=spd_x, spd_y=-6))
+
+
+
 
